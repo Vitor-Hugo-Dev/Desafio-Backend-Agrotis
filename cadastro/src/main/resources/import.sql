4b102b0f05e260e7a948c3eb425b1bccb461b393
vitor hugo da silva <vitorhugo.dev1997@gmail.com>
2022-05-27 05:48:44 -0300

    feat: primeira população do banco

---
+++ cadastro/src/main/resources/import.sql
@@ -0,0 +1,15 @@
+INSERT INTO tb_laboratorio(name) VALUES ('Agro Skynet');
+INSERT INTO tb_laboratorio(name) VALUES ('Umbrella Agro');
+INSERT INTO tb_laboratorio(name) VALUES ('Osborn Agro');
+INSERT INTO tb_laboratorio(name) VALUES ('Skyrim Agro');
+INSERT INTO tb_laboratorio(name) VALUES ('Agro Brasil');
+
+INSERT INTO tb_propriedade(name, cnpj) VALUES ('Agrotis 1', '04.909.987/0001-89');
+INSERT INTO tb_propriedade(name, cnpj) VALUES ('Agrotis 2', '04.909.987/0001-88');
+INSERT INTO tb_propriedade(name, cnpj) VALUES ('Agrotis 3', '04.909.987/0001-87');
+INSERT INTO tb_propriedade(name, cnpj) VALUES ('Agrotis 4', '04.909.987/0001-86');
+INSERT INTO tb_propriedade(name, cnpj) VALUES ('Agrotis 5', '04.909.987/0001-85');
+INSERT INTO tb_propriedade(name, cnpj) VALUES ('Agrotis 6', '04.909.987/0001-84');
+
+
+
