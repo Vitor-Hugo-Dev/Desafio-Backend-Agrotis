7094b84b28f0bda6b0afc2fb05c7de0be0fa1c63
vitor hugo da silva <vitorhugo.dev1997@gmail.com>
2022-05-29 20:22:46 -0300

    feat: popula a tabela de serviços

--- cadastro/src/main/resources/import.sql
+++ cadastro/src/main/resources/import.sql
@@ -11,5 +11,6 @@ INSERT INTO tb_propriedade(name, cnpj) VALUES ('Agrotis 4', '04.909.987/0001-86'
 INSERT INTO tb_propriedade(name, cnpj) VALUES ('Agrotis 5', '04.909.987/0001-85');
 INSERT INTO tb_propriedade(name, cnpj) VALUES ('Agrotis 6', '04.909.987/0001-84');
 
-
-
+INSERT INTO tb_servico(description, initial_date, final_date, laboratorio_id, propriedade_id, usuario) VALUES ('serviço 1', '2022-02-02T17:41:44Z', '2022-02-02T17:41:44Z', 1, 1, 'John Connor');
+INSERT INTO tb_servico(description, initial_date, final_date, laboratorio_id, propriedade_id, usuario) VALUES ('serviço 2', '2022-02-02T17:41:44Z', '2022-02-02T17:41:44Z', 2, 2, 'Leon Scott Kennedy');
+INSERT INTO tb_servico(description, initial_date, final_date, laboratorio_id, propriedade_id, usuario) VALUES ('serviço 3', '2022-02-02T17:41:44Z', '2022-02-02T17:41:44Z', 4, 4, 'DragonBorn Agricultor');
